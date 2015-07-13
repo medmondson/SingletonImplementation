bd6ef92b38d660b29d54d6755621460b05adac7a
eddy556 <eddy556@gmail.com>
2015-07-14 00:02:05 +0100

    Lazy loading singleton code

---
+++ Singleton/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Singleton firstthing = Singleton.Instance;
+            Console.WriteLine(firstthing._timeCreated);
+
+            Thread.Sleep(100);
+
+            Singleton secondthing = Singleton.Instance;
+            Console.WriteLine(secondthing._timeCreated);
+
+            Thread.Sleep(800);
+
+            Singleton thirdthing = Singleton.Instance;
+            Console.WriteLine(thirdthing._timeCreated);
+
+            Thread.Sleep(300);
+
+            Singleton fourththing = Singleton.Instance;
+            Console.WriteLine(fourththing._timeCreated);
+
+            Thread.Sleep(1000);
+
+            Singleton fifththing = Singleton.Instance;
+            Console.WriteLine(fifththing._timeCreated);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        private static readonly Lazy<Singleton> lazy = new Lazy<Singleton>(() => new Singleton());
+        public static Singleton Instance { get { return lazy.Value; } }
+
+        private Singleton()
+        {
+        }
+
+        //Time
+        public readonly DateTime _timeCreated = DateTime.Now;
+
+        //Ninject kernel
+        public readonly IKernel kernel = new StandardKernel();
+    }
+}
